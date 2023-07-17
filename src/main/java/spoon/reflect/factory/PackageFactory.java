46a2ae56b1bef7f4fcfc96c2b3ef5187bb7f19be
Raghav Shankar <raghav@deepsource.io>
2023-07-17 11:34:27 +0530

    fix: qodana error

--- src/main/java/spoon/reflect/factory/PackageFactory.java
+++ src/main/java/spoon/reflect/factory/PackageFactory.java
@@ -8,6 +8,7 @@
 package spoon.reflect.factory;
 
 
+import spoon.annotations.Nullable;
 import spoon.reflect.declaration.CtModule;
 import spoon.reflect.declaration.CtPackage;
 import spoon.reflect.declaration.CtPackageDeclaration;
@@ -199,6 +200,7 @@ public class PackageFactory extends SubFactory {
 	 * @param mergingPackage - The package to merge everything into.
 	 * @return
 	 */
+	@Nullable
 	private CtPackage mergeAmbiguousPackages(ArrayList<CtPackage> packagesToMerge, CtPackage mergingPackage) {
 
 		if (mergingPackage == null) return null;
